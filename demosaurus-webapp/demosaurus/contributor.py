41548e41669a715bf879e504b59f273296974426
Sara Veldhoen <sara.veldhoen@kb.nl>
2020-03-03 15:13:03 +0100

    Added links to external author descriptions to author page

--- demosaurus-webapp/demosaurus/contributor.py
+++ demosaurus-webapp/demosaurus/contributor.py
@@ -13,20 +13,16 @@ bp = Blueprint('contributor', __name__)
 def authorpage(id):
     db = get_db()
     author = db.execute(
-        ' SELECT *'
-        ' FROM contributor'
-        ' WHERE contributor.ppn = ?',
-        (id,)
-    ).fetchone()
-
-    # publications = db.execute(
-    #      ' SELECT *'
-    #      ' FROM publication'
-    #      ' JOIN authorship'
-    #      ' ON authorship.publication_isbn == publication.isbn'
-    #      ' WHERE authorship.contributor_ppn = ?',
-    #      (id,)
-    #  ).fetchall() 
+        ' SELECT *, '
+        '        (SELECT identifier FROM VIAF WHERE ppn = ? LIMIT 1) AS VIAF,'
+        '        (SELECT identifier FROM ISNI WHERE ppn = ? LIMIT 1) AS ISNI,'
+        '        (SELECT identifier FROM Wikipedia '
+        '                LEFT JOIN Wiki_languages ON Wikipedia.language = Wiki_languages.language'
+        '                WHERE Wikipedia.ppn = ?'
+        '                ORDER BY -Wiki_languages.rank desc LIMIT 1) AS WIKI'
+        ' FROM contributor WHERE contributor.ppn = ?',
+        (id,id,id,id)
+    ).fetchone()  
     
     return render_template('contributor/authorpage.html', author=author, publications = [])
 
