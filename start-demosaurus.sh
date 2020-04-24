2887a68afeedc036ba6880975f2e150ec95b2f0b
Sara Veldhoen <sara.veldhoen@kb.nl>
2020-04-24 11:10:18 +0200

    Added script to sync database and start dev Flask environment

---
+++ start-demosaurus.sh
@@ -0,0 +1,17 @@
+. venv/bin/activate
+cd demosaurus-webapp
+if (($#==0 ));
+	then 
+	test -f ../Surfdrive/demosaurus.sqlite \
+		&& echo "Syncing database from Surfdrive" \
+		&& rsync ../Surfdrive/demosaurus.sqlite instance/demosaurus.sqlite \
+		|| ( flask init-db && echo "No database in Surfdrive. Instantiated new database." )
+else
+ 	echo "Not syncing database."
+	test -f instance/demosaurus.sqlite && echo "Database exists." || (flask init-db "Instantiated new database.")
+fi
+
+export FLASK_APP=demosaurus
+export FLASK_ENV=development
+
+flask run
