a5ad1bf2f64222ea8b3f6e9b55e5b7d29dd390e4
Thomas <_@thomashaighton.com>
2020-12-11 18:03:07 +0100

    First commit, Changed dependencie management to venv instead of pipenv, modified Readme

--- start-demosaurus.sh
+++ start-demosaurus.sh
@@ -1,9 +1,9 @@
 cd demosaurus-webapp
 if (($#==0 ));
 	then 
-	test -f ../Surfdrive/demosaurus.sqlite \
+	test -f ~/surfdrive/Demosaurus/demosaurus.sqlite \
 		&& echo "Syncing database from Surfdrive" \
-		&& rsync ../Surfdrive/demosaurus.sqlite instance/demosaurus.sqlite \
+		&& rsync ~/surfdrive/Demosaurus/demosaurus.sqlite instance/demosaurus.sqlite \
 		|| ( flask init-db && echo "No database in Surfdrive. Instantiated new database." )
 else
  	echo "Not syncing database."
