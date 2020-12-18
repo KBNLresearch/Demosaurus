ae33508075f86dbe7c5475e8c9e9220333e00c45
Sara Veldhoen <sara.veldhoen@kb.nl>
2020-12-18 17:13:35 +0100

    Obtaining information on subject indexing of the publication, plus some small readibility fixes

--- demosaurus-webapp/demosaurus/publication.py
+++ demosaurus-webapp/demosaurus/publication.py
@@ -9,21 +9,10 @@ from demosaurus.db import get_db
 
 bp = Blueprint('publication', __name__)
 
-@bp.context_processor
-def utility_processor():    
-    def list_of_roles():
-        db = get_db()
-        roles = db.execute(
-            ' SELECT author_rolesID, legible, ggc_code'
-            ' FROM authorship_roles'
-        ).fetchall()
-        print(roles)
-        return json.dump(list_of_roles)
-    return dict(list_of_roles=list_of_roles)
-
 @bp.route('/<id>/view')
 def view(id):
     db = get_db()
+    # Obtain data on publication & contributors from the database
     publication = db.execute(
         ' WITH annotations AS ('
         '     SELECT publication_ppn, group_concat(annotation) AS annotations from publication_annotations'
@@ -38,7 +27,7 @@ def view(id):
         (id,id)
     ).fetchone()
 
-    print(publication.keys)
+    print(publication.keys())
 
     contributors = db.execute(
         ' SELECT *'
@@ -66,20 +55,36 @@ def view(id):
         cover_location = None
 
 
-    print(len(contributors),  'contributor records')
-    try: print(contributors[0].keys())
-    except: True
+    subjects = db.execute(
+        ' WITH ranks AS (SELECT DISTINCT rank from publication_brinkman)'
+        ' SELECT ranks.rank, '
+        ' CBK_genre AS CBK_genre_id, genre AS CBK_genre, '
+        ' CBK_thema, '
+        ' brinkman AS brinkman_id, term AS brinkman'
+        ' FROM ranks'
+        ' LEFT JOIN publication_CBK_genre t1 ON t1.publication_ppn =:ppn AND ranks.rank =t1.rank '
+        ' LEFT JOIN thesaurus_CBK_genres t1a ON t1a.identifier = t1.CBK_genre  '
+        ' LEFT JOIN publication_CBK_thema t2 ON t2.publication_ppn =:ppn AND ranks.rank =t2.rank '
+        ' LEFT JOIN publication_brinkman  t3 ON t3.publication_ppn =:ppn AND ranks.rank =t3.rank '
+        ' LEFT JOIN thesaurus_brinkmantrefwoorden t3a ON t3a.ppn = t3.brinkman',
+        {'ppn':id}
+        ).fetchall()
+    print(subjects[0].keys())
 
-    return render_template('publication/view.html', publication = publication, cover = cover_location, contributors=contributors, role_list=roles_options)
+    # Serve the list to the client: render the template with the obtained data
+    return render_template('publication/view.html', publication = publication, cover = cover_location, contributors=contributors, subjects = subjects, role_list=roles_options)
 
 
 
 @bp.route('/')
 def overview():
     db = get_db()
+    # Select a random subset of publications from the test set to serve as examples
     publications = db.execute(
-        ' SELECT publication_ppn, titelvermelding, verantwoordelijkheidsvermelding'
+        ' SELECT publication_basicinfo.publication_ppn, titelvermelding, verantwoordelijkheidsvermelding'
         ' FROM publication_basicinfo'
+        ' JOIN publication_datasplits ON publication_datasplits.publication_ppn = publication_basicinfo.publication_ppn'
+        ' WHERE publication_datasplits.datasplit like \"test\"'
         ' ORDER BY RANDOM() LIMIT 20'
     ).fetchmany(20)
     return render_template('publication/overview.html', publications=publications)
