26f8fb9d1e9d2ae544f14e935cfdd6290d2b75c3
Sara Veldhoen <sara.veldhoen@kb.nl>
2021-07-27 16:12:24 +0200

    Improved call to thesaureer, added jvu and proper title, some cleaning up

--- demosaurus-webapp/demosaurus/static/js/contributor-block.js
+++ demosaurus-webapp/demosaurus/static/js/contributor-block.js
@@ -1,8 +1,8 @@
 var focus_index;
 var main_author = true;
 
-function init(){
-  document.getElementById("contributors_tab").click();
+function init_contributors(){
+
   if (contributors.length < 1){
     add_contributor_row();
   }
@@ -56,22 +56,28 @@ function deactivate_rows() {
 }
 
 function thesaureer(index){
-  var contributorname = $("#aut_name_" + index).val();
+  activate_row(index);
   try {var role = $('#role_'+index).val().match(/\[(.*?)\]/)[1];}
   catch(e) {var role = null; }
+  var contributor_name =  $("#aut_name_" + index).val();
+  var data = {
+           'contributor_name' : contributor_name,
+           'contributor_role' : role,
+           'publication_title': $('#publication_title').text(),
+           'publication_genres': JSON.stringify(genres),
+           'publication_year': $('#publicationYear').text()
+         };
 
-  activate_row(index);
+  console.log('Thesaureer');
+  console.log(data);
+
 
   $("#candidate_list > tbody").empty()
-  $('#thesaureer_title').text('NTA-records voor '+contributorname);
+  $('#thesaureer_title').text('NTA-records voor '+contributor_name);
 
   $.ajax({
     url: '/thesaureer',
-    data: {'contributor_name' : contributorname,
-          'contributor_role' : role,
-           'publication_title': $('#publication_title').val(),
-           'publication_genres': JSON.stringify(genres)
-         },
+    data: data,
     dataType: 'json',
     context:this,
     success: function(response){
@@ -83,87 +89,4 @@ function thesaureer(index){
   });
 }
 
-function export_info() {
-  console.log('Export button')
 
-  deactivate_rows();
-  $("#export > #message").empty();
-
-  var allroles = true; 
-  var allppns = true; 
-
-  var contributors = []; 
-  var all_kmcs = '';
-
-  // build up kmc contents for contributors: contributorname$role$!ppn!viafname
-  var rows = $('#contributortable > tbody > tr');
-  var at_kmc = 3011;
-  for (var i=0; i < rows.length; i++) {
-    var id = rows[i].id.split('_')[1];
-    console.log('row', id);
-
-    console.log($('#main_'+id).is(':checked'));
-
-
-
-    if (i==0 && $('#main_'+id).is(':checked')){
-      all_kmcs += '<p>3000\t';
-    }
-    else {
-      all_kmcs += "<p>"+at_kmc+"\t";
-      if (at_kmc <3019) {
-        at_kmc ++;
-      }
-    }
-
-    all_kmcs += $('#aut_name_'+id).val();
-    role = $('#role_'+id).val();
-    if (role) { 
-        role = role.replace(/(^.*\[|\].*$)/g, ''); // get the role-code bit
-        all_kmcs += '$'+ role +'$';
-      }
-      else {
-        $('#role_'+id).css("backgroundColor","red");
-        allroles = false; 
-      }
-
-      ppn = $('#ppn_'+id).val();
-      if (ppn) {
-        all_kmcs += '!'+ ppn +'!';
-      }
-      else {
-        $('#ppn_'+id).css("backgroundColor","red");
-        allppns = false; 
-      }
-      all_kmcs += "</p>";
-    }
-    
-    $('#contributors_tab_flag').css("visibility","hidden");
-
-    // Report about the completeness of the input
-    if (! allroles) {
-        $('#contributors_tab_flag').css("visibility","visible");
-        $('#export > #message').append('<br><i>&#8226; Let op: niet bij alle auteurs is de rol ingevoerd!</i></br>');
-      }
-    if (! allppns) {
-        $('#contributors_tab_flag').css("visibility","visible");
-        $('#export > #message').append('<br><i>&#8226; Let op: niet alle auteurs zijn gethesaureerd!</i></br>');
-      }
-
-      $('#thesaureer_title').text('KMCS:');
-      //$("#candidate_list > thead").empty();    
-      //$("#candidate_list > tbody").empty();
-
-    
-    // Serve collected information in the web application
-    // NB todo: export primary author (if they exist) to KMC 3000 rather than 3011
-    $('#export_content').html(all_kmcs);
-  }
-
-  /*
-  function suggest_topics() {
-    console.log('Annif button clicked')
-    $("#candidate_lis").empty();    
-    $('#placeholder').html('Annif API not connected');
-  }
-  */
