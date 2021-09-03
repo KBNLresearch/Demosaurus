cfab4f7b0f5c7e419395cf0854d47ea601f16a97
Sara Veldhoen <sara.veldhoen@kb.nl>
2021-09-03 09:46:59 +0200

    Removed WIP user-login stuff

--- demosaurus-webapp/demosaurus/__init__.py
+++ demosaurus-webapp/demosaurus/__init__.py
@@ -2,10 +2,6 @@ import os
 
 from flask import Flask, render_template, request, redirect, send_from_directory
 from flask_jsglue import JSGlue
-from flask_login import login_required, current_user, login_user, logout_user
-from demosaurus.models import UserModel, db_users
-from demosaurus.auth import login_manager
-
 
 def create_app(test_config=None, SECRET_KEY='dev'):
     # create and configure the app
@@ -16,14 +12,6 @@ def create_app(test_config=None, SECRET_KEY='dev'):
     )
     jsglue = JSGlue()
     jsglue.init_app(app)
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    db_users.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = 'login'
-
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -66,59 +54,8 @@ def create_app(test_config=None, SECRET_KEY='dev'):
         return send_from_directory(os.path.join(app.root_path, 'static'),
                                    'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-    # User login stuff.
-    @app.before_first_request
-    def create_all():
-        db_users.create_all()
-
-    # Werkt niet? Je wil inloggen wanneer je op de "homepage" (root) beland.
     @app.route('/')
-    @login_required
     def index():
         return render_template('/')
-
-    # Method for login view.
-    @app.route('/login', methods = ['POST', 'GET'])
-    def login():
-        if current_user.is_authenticated:
-            return redirect('/')
-
-        if request.method == 'POST':
-            email = request.form['email']
-            user = UserModel.query.filter_by(email=email).first()
-            if user is not None and user.check_password(request.form['password']):
-                login_user(user)
-                return redirect('/')
-
-        return render_template('login.html')
-
-    # Register new user method.
-    @app.route('/register', methods=['POST', 'GET'])
-    def register():
-        if current_user.is_authenticated:
-            return redirect('/')
-
-        if request.method == 'POST':
-            email = request.form['email']
-            username = request.form['username']
-            password = request.form['password']
-
-            if UserModel.query.filter_by(email=email).first():
-                return ('Email already Present')
-
-            user = UserModel(email=email, username=username)
-            user.set_password(password)
-            db_users.session.add(user)
-            db_users.session.commit()
-            return redirect('/login')
-        return render_template('register.html')
-
-    # Logout method.
-    @app.route('/logout')
-    @login_required
-    def logout():
-        logout_user()
-        return redirect('/login')
-
 
     return app
