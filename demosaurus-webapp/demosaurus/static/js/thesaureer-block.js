7e1c42d6379550a1e76dfaa2ff37c6a9abd59852
Thomas <mail@thomashaighton.com>
2021-09-12 10:30:11 +0200

    'verwijder rij' nu werkend

--- demosaurus-webapp/demosaurus/static/js/thesaureer-block.js
+++ demosaurus-webapp/demosaurus/static/js/thesaureer-block.js
@@ -56,7 +56,7 @@ function candidate_note(candidaterow){
               context['id']=response[i].author_ppn;
 
               $("#candidate_list > tbody").append($('<tr>')
-                .append($('<td>').append('<input onclick="delete_row(this);" type="button" value="&#xf2ed;" class="fas fa-trash-alt" title="Verwijder naam" padding="0px">'))
+                .append($('<td>').append('<button value="&#xf2ed;" class="fas fa-trash-alt" title="Verwijder naam"/>'))
                 .append($('<td class="ppn_cell" >')
                   .append($('<a class="action" title="Selecteer naam" href="#" onclick="choose_ppn(\''+response[i].author_ppn+'\')"; return false;>')
                     .text(response[i].author_ppn)))
@@ -114,8 +114,17 @@ function candidate_note(candidaterow){
             }
           );
 
-          var cl = $('#candidate_list').DataTable({ordering: false});
-          }
+
+          $('#candidate_list').DataTable({ordering: false});
+          
+          $('#candidate_list').on('click', '.fas.fa-trash-alt', function(){
+            var cl = $('#candidate_list').DataTable();
+            cl
+              .row($(this).parents('tr'))
+              .remove()
+              .draw();
+          });
+        }
     };
 
 function choose_ppn(ppn) {
