5e1663a2205700aa6a626f77135e3d246652122e
Sara Veldhoen <sara.veldhoen@kb.nl>
2021-09-23 11:21:45 +0200

    DB logging off

--- demosaurus-webapp/demosaurus/db.py
+++ demosaurus-webapp/demosaurus/db.py
@@ -9,7 +9,7 @@ def get_db():
             current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
-        g.db.set_trace_callback(print) # logs in print
+        #g.db.set_trace_callback(print) # logs in print
         g.db.row_factory = sqlite3.Row
     return g.db
 
