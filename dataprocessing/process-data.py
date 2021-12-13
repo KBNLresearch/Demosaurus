f8b803c2d594c451938af44d4a2f3844d2b6a7af
Sara Veldhoen <sara.veldhoen@kb.nl>
2021-12-13 11:29:55 +0100

    Improved code fot datasplits (with heldout)

--- dataprocessing/process-data.py
+++ dataprocessing/process-data.py
@@ -7,20 +7,26 @@ import views_and_indices
 import os
 import numpy as np
 
-def ingest_publication_data(file_name, new_datasplits = False, dataset_id = -1):
+def ingest_publication_data(file_name):
     assert os.path.exists(file_name)
     NBD_fiction = pd.read_csv('data/NBD-pilotdata/kb_fiction_ppns.txt', names = ['ppn'])
     ppn_list = list(NBD_fiction['ppn'])
     # obtain publication records: create pandas dataframes
     dfs = read_pica.obtain_dataframes(file_name, ppn_list=ppn_list)
-    if new_datasplits:
-        datasplits = dfs['publication_basicinfo'][['publication_ppn']]
-        datasplits['dataset_id']=dataset_id
-        datasplits['datasplit_id'] = (np.random.uniform(size=len(datasplits))>3/4).astype(int)
-        dfs['publication_datasplits'] = datasplits
     for table_name, table in dfs.items():
         print('Obtain', table_name)
     return dfs
+
+def create_datasplits(set_name, dataset_id, heldout_file = ''):
+    df = csv_db.import_csv('publication_basicinfo', check_schema=False, postfix='_'+set_name)[['publication_ppn']]
+    df['dataset_id'] = dataset_id
+    if heldout_file:
+        with open(heldout_file, 'r') as f:
+            heldout_ppns = f.read().splitlines()
+        df.loc[df.publication_ppn.isin(heldout_ppns),'datasplit_id'] = 2
+    tosplit = df.datasplit_id.isna()
+    df.loc[tosplit, 'datasplit_id'] = (np.random.uniform(size=sum(tosplit)) > 3 / 4).astype(int)  # train (0) and test (1)
+    csv_db.export_csv('publication_datasplits', df, postfix='_'+set_name if set_name else '')
 
 def apply_preprocessing(dfs, postfix = ''):
     #dfs['publication_basicinfo'] = preprocessing.normalize_publishers(dfs['publication_basicinfo'], postfix)
@@ -35,11 +41,12 @@ def update_publisher_table(publisher_map):
     #df.append()
     return df
 
-def process_publication_info(set_name, dataset_id):
+def process_publication_info(set_name):
     # Ingest publication info and write to DB
     fname = 'data/NBD-pilotdata/picaplus_kb_ebooks_20210927.XML'
-    dfs = ingest_publication_data(fname, new_datasplits=True, dataset_id=dataset_id)
+    dfs = ingest_publication_data(fname)
     dfs = apply_preprocessing(dfs, postfix='_'+set_name if set_name else '')
+
     for table_name, df in dfs.items():
         csv_db.export_csv(table_name, df, postfix='_'+set_name if set_name else '')
 
@@ -64,14 +71,21 @@ if __name__ == '__main__':
     #process_thesauri()
 
     # Ingest and preprocess publication information: store in CSV files
-    #process_publication_info(set_name='NBD', dataset_id=1)
+    #process_publication_info(set_name='NBD')
+
+    # Split into train, test, heldout
+    create_datasplits(set_name='NBD', dataset_id=1,
+                      heldout_file = 'data/NBD-pilotdata/KB_validatieset_def.csv')
+
+
 
     # Obtain information from CSVs and write to database
-    #for table in csv_db.schemata.keys():
-    #    csv_db.fill_table(table,db='data/demosaurus_NBD.sqlite', overwrite=True, postfix = '_NBD')
+    for table in csv_db.schemata.keys():
+        if table != 'publication_datasplits': continue
+        csv_db.fill_table(table,db='data/demosaurus_NBD.sqlite', overwrite=True, postfix = '_NBD')
 
     # Build indices (+FTS5) and views for regular entries
-    views_and_indices.create_views(db='data/demosaurus_NBD.sqlite')
-    views_and_indices.create_indices(db='data/demosaurus_NBD.sqlite')
+    #views_and_indices.create_views(db='data/demosaurus_NBD.sqlite')
+    #views_and_indices.create_indices(db='data/demosaurus_NBD.sqlite')
 
 
